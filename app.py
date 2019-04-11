68bb605de47f6d9c8f1f5c0378e08f11025126f0
Ashley McGee <6312276+AshleyMcGee@users.noreply.github.com>
2019-04-11 18:43:40 -0500

    Change slugs to basic names

--- app.py
+++ app.py
@@ -2,32 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/landing.html')
+@app.route('/')
 def home():
     return render_template("landing.html")
 
 
-@app.route('/temp_x.html')
+@app.route('/temp')
 def temps():
     return render_template("temp_x.html")
 
-@app.route('/humid_x.html')
+@app.route('/humid')
 def humidity():
     return render_template("humid_x.html")
 
-@app.route('/clouds_x.html')
+@app.route('/clouds')
 def clouds():
     return render_template("clouds_x.html")
 
-@app.route('/winds_x.html')
+@app.route('/winds')
 def winds():
     return render_template("winds_x.html")
 
-@app.route('/comparisons.html')
+@app.route('/comparisons')
 def comparisons():
     return render_template("comparisons.html")
 
-@app.route('/data.html')
+@app.route('/data')
 def data_used():
     return render_template("data.html")
 
